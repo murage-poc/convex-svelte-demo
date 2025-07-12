70d13ffb54b3be7c22f2889e747a7a558d92ee76
Murage <25152892+0xMurage@users.noreply.github.com>
2025-07-12 18:01:26 +0300

    feat: add and set up convex

--- demo/src/routes/+layout.svelte
+++ demo/src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
+	import { PUBLIC_CONVEX_URL } from '$env/static/public';
+	import { setupConvex } from 'convex-svelte';
 	import '../app.css';
+	
+	setupConvex(PUBLIC_CONVEX_URL,{});
 	
 	let { children } = $props();
 </script>
